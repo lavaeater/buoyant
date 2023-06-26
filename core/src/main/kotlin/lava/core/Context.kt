cd639dc4457095ad12f0d38576643ab280b7ea23
Tommie Nygren <tommie.nygren@gmail.com>
2023-06-27 00:24:21 +0200

    Fixed some level stuff

--- core/src/main/kotlin/lava/core/Context.kt
+++ core/src/main/kotlin/lava/core/Context.kt
@@ -56,7 +56,7 @@ object Context : InjectionContext() {
                 setContactListener(CollisionManager(game))
             })
             bindSingleton(RayHandler(inject()).apply {
-                setAmbientLight(.1f)
+                setAmbientLight(.25f)
                 setBlurNum(3)
             })
             bindSingleton(ShapeDrawer(inject<PolygonSpriteBatch>() as Batch, shapeDrawerRegion))
