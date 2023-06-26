8a3bae98e24172404485b31d4808b3fd242d4a2a
Tommie Nygren <tommie.nygren@gmail.com>
2023-06-26 13:19:28 +0200

    Camera is indeed completely insane now

---
+++ core/src/main/kotlin/lava/ecs/systems/CrazyCameraSystem.kt
@@ -0,0 +1,28 @@
+package lava.ecs.systems
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Vector2
+import ktx.math.vec3
+import twodee.ecs.ashley.components.TransformComponent
+import twodee.ecs.ashley.systems.CameraFollowSystem
+
+class CrazyCameraSystem(camera: OrthographicCamera, alpha: Float) : CameraFollowSystem(camera, alpha) {
+
+    private val directionVector = Vector2.Y.cpy()
+    private val cameraUp = vec3(0f, 0f, 0f)
+        get() = field.set(directionVector.x, directionVector.y, 0f)
+
+    override fun processEntity(entity: Entity, deltaTime: Float) {
+        val position = TransformComponent.get(entity).position
+        directionVector.setAngleDeg(TransformComponent.get(entity).angleDegrees)
+        cameraPosition.set(position)
+
+        camera.position.lerp(
+            vec3(cameraPosition, 0f), alpha
+        )
+        camera.up.lerp(cameraUp, alpha)
+
+        camera.update(true)
+    }
+}
