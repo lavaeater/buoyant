099ba918b144270e682a31bef0143797a0774aa2
Tommie Nygren <tommie.nygren@gmail.com>
2023-06-28 15:22:21 +0200

    Changed bubble wiggling a bit

--- core/src/main/kotlin/lava/ecs/systems/BubbleBuoyancySystem.kt
+++ core/src/main/kotlin/lava/ecs/systems/BubbleBuoyancySystem.kt
@@ -3,6 +3,7 @@ package lava.ecs.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
 import ktx.ashley.allOf
+import ktx.math.plus
 import ktx.math.times
 import ktx.math.vec2
 import lava.ecs.components.BubbleComponent
@@ -11,15 +12,13 @@ import kotlin.math.pow
 
 class BubbleBuoyancySystem:IteratingSystem(allOf(BubbleComponent::class, Box2d::class).get()) {
     val directionVector = vec2(0f,1f)
+    val left = vec2(-1f, 0f)
+    val right = vec2(1f, 0f)
     override fun processEntity(entity: Entity, deltaTime: Float) {
-        val angle = directionVector.angleDeg() - 90f
-        if(angle > 0f) {
-            directionVector.rotateDeg((-25..0).random().toFloat())
-        } else {
-            directionVector.rotateDeg((0..25).random().toFloat())
-        }
+        val forceVector = if((1..2).random() ==1) directionVector + left else directionVector + right
+
         val box2d = Box2d.get(entity)
-        box2d.body.applyForceToCenter(directionVector, true)
+        box2d.body.applyForceToCenter(forceVector, true)
 
         val velDir = box2d.body.linearVelocity.cpy()
         val velMag = velDir.len()
